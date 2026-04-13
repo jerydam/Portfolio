f71c3e7d4ed6f443acf50f1c155012ecfd976b4d
jerydam <jerydan148@gmail.com>
2026-04-13 23:44:13 +0100

    order

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,6 @@ import { motion } from "framer-motion";
 
 import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
-import { ErrorBoundary } from "./ErrorBoundary";
 
 const Hero = () => {
   return (
@@ -24,11 +23,9 @@ const Hero = () => {
         </div>
       </div>
 
-     <ErrorBoundary fallback={<div className="w-full h-full bg-primary" />}>
-  <ComputersCanvas />
-</ErrorBoundary>
+      <ComputersCanvas />
 
-      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
+      <div className='absolute xs:bottom-10npm run dev bottom-32 w-full flex justify-center items-center'>
         <a href='#about'>
           <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
             <motion.div
